993b5abe66f1d9269acac24c2b944d1b44576b40
Subhadip Mahapatra <subhadipmahapatra56@gmail.com>
2026-02-13 01:25:08 +0530

    Time: 717 ms (43.81%), Space: 0B (100%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT p.product_name,
+       SUM(o.unit) AS unit
+FROM Orders o
+JOIN Products p
+ON o.product_id = p.product_id
+WHERE order_date >= '2020-02-01'
+  AND order_date < '2020-03-01'
+GROUP BY p.product_id, p.product_name
+HAVING SUM(o.unit) >= 100;
